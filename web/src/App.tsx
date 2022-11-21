712bd9958d1f8c6c9264e55143c72884685cd64c
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2022-11-21 20:59:23 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GlobalStyles } from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
