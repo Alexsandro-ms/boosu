f12a59e9177a084afed170f1f54a70c8bf2cd314
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2022-11-26 21:53:53 -0300

    fix(index): fixed conflict with node:path and node:http
    Fixed conflict with improper node:path and node:http imports for production

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,23 +3,9 @@ import { router } from './routes';
 import { config } from 'dotenv';
 import mongoose from 'mongoose';
 
-<<<<<<< HEAD
-import { createServer } from 'node:http';
-import { resolve } from 'node:path';
-
-import { router } from './routes';
-
-=======
->>>>>>> parent of 88f902c (feat(index and created orders): connection with socket by the api)
 const app = express();
 const dotEnvConfig = config();
 
-<<<<<<< HEAD
-const server = createServer(app);
-export const io = new Server(server);
-
-=======
->>>>>>> parent of 88f902c (feat(index and created orders): connection with socket by the api)
 mongoose
   .connect(`${process.env.MONGOOSE_URL}`)
   .then(() => {
@@ -35,18 +21,8 @@ mongoose
       next();
     });
 
-<<<<<<< HEAD
-    app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
-
-    app.use(express.json());
-    app.use(router);
-
-    server.listen(PORT, () =>
-      console.log(`🚀... server is running on http://localhost:${PORT}`)
-=======
     app.listen(PORT, () =>
       console.log(`🚀 server is running on http://localhost:${PORT}`)
->>>>>>> parent of 88f902c (feat(index and created orders): connection with socket by the api)
     );
   })
   .catch((error) => console.log('Erro:', error));
