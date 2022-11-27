7c6d76427e16708c86eb5f45219d869b16e4055f
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2022-11-27 16:11:32 -0300

    refactor(index api): refactored index code in api folder

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,18 +1,22 @@
 import express from 'express';
+import path from 'node:path';
+import http from 'node:http';
+import mongoose from 'mongoose';
 import { router } from './routes';
 import { config } from 'dotenv';
-import mongoose from 'mongoose';
+
+import { Server } from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 const dotEnvConfig = config();
 
 mongoose
   .connect(`${process.env.MONGOOSE_URL}`)
   .then(() => {
-    const PORT = process.env.PORT || 8080;
-
-    app.use(express.json());
-    app.use(router);
+    const PORT = process.env.PORT;
 
     app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*');
@@ -20,6 +24,9 @@ mongoose
       res.setHeader('Access-Control-Allow-Headers', '*');
       next();
     });
+
+    app.use(express.json());
+    app.use(router);
 
     app.listen(PORT, () =>
       console.log(`🚀 server is running on http://localhost:${PORT}`)
