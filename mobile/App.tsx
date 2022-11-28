78587724d5d3eb0b522867466f651b168d579e2c
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2022-11-28 19:22:57 -0300

    feat(app mobile): loading app fonts with useFonts and added dark themed status bar
    rendering font general sans from useFonts, if not loaded, it will return null

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,20 @@
+import { useFonts } from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
 import Main from './src/main';
 
 export default function App() {
+  const [isFontLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!isFontLoaded) return null;
+
   return (
     <>
       <Main />
+      <StatusBar style="dark" />
     </>
   );
 }
