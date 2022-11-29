433c8a310110d5ed38b099a7edae11bfbfa642e8
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2022-11-29 19:01:05 -0300

    feat(main): created util object and connected to the api
    api object with api base url, request made for 'categories' and 'products' endpoints

--- mobile/src/main/index.tsx
+++ mobile/src/main/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// Funções uteis
+import { api } from '../utils/api';
+// Componentes e estilos
 import { Header } from '../components/Header';
+import { Menu } from '../components/Menu';
+import { Container } from './styles';
+// Tipagem de dados retornados da api
+import { Category } from '../types/Category';
+import { Product } from '../types/Product';
 
 function Main() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    Promise.all([api.get('/categories'), api.get('/products')])
+      .then(([categoriesResponse, productsResponse]) => {
+        setCategories(categoriesResponse.data);
+        setProducts(productsResponse.data);
+        setIsLoading(false);
+      })
+      .catch((error) => alert(`Erro: ${error}`));
+  }, []);
   return (
-    <>
+    <Container>
       <Header />
-    </>
+      <Menu products={products} />
+    </Container>
   );
 }
 
