33e7fd39784164542cc0d2672b0a364ba465f1a8
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2022-12-01 18:47:50 -0300

    refactor(header): added function to cancel order

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -4,11 +4,11 @@ import { Container, OrderContent, OrderHeader, Table } from './styles';
 
 interface HeaderProps {
   selectedTable?: string;
-  // onCancelOrder: () => void;
+  onCancelOrder: () => void;
 }
 
 // export function Header({ selectedTable, onCancelOrder }: HeaderProps) {
-export function Header({ selectedTable }: HeaderProps) {
+export function Header({ selectedTable, onCancelOrder }: HeaderProps) {
   return (
     <Container>
       {!selectedTable && (
@@ -27,7 +27,7 @@ export function Header({ selectedTable }: HeaderProps) {
             <Text size={24} weight="600">
               Pedido
             </Text>
-            <TouchableOpacity onPress={() => alert('Click')}>
+            <TouchableOpacity onPress={onCancelOrder}>
               <Text color="#D73035" weight="600" size={14}>
                 Cancelar pedido
               </Text>
